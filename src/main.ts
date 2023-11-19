d678c6c739e9d7929a9e5ecb1490d861789cd19a
Viacheslav Biedovskyi <biedovskyiv@mydigicode.com>
2023-11-19 13:51:39 -0800

    add swagger and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET'],
+  });
+
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Wizard Cities')
+    .setDescription('Wizard Cities docs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
